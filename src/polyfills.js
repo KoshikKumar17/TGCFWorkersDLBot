8fe8a1aa9b56ff851155261acca872ea09015ee1
Hash Hackers <87309724+PBhadoo@users.noreply.github.com>
2026-03-12 11:30:18 +0530

    Fix setTimeout.unref() not a function in browser
    teleproto calls setTimeout(...).unref() which is Node.js-only.
    Patch globalThis.setTimeout/setInterval to return objects with
    stub unref()/ref() methods for browser compatibility.

--- src/polyfills.js
+++ src/polyfills.js
@@ -17,3 +17,36 @@ if (typeof globalThis.process === 'undefined') {
     nextTick: (fn, ...args) => setTimeout(() => fn(...args), 0),
   };
 }
+
+// Patch setTimeout/setInterval to add .unref() (Node.js-only method)
+// teleproto uses setTimeout(...).unref() which doesn't exist in browsers
+const _origSetTimeout = globalThis.setTimeout;
+const _origSetInterval = globalThis.setInterval;
+globalThis.setTimeout = function(fn, ms, ...args) {
+  const id = _origSetTimeout.call(this, fn, ms, ...args);
+  if (typeof id === 'number') {
+    // Return an object with unref/ref stubs for browser compatibility
+    return { _id: id, unref() { return this; }, ref() { return this; }, [Symbol.toPrimitive]() { return this._id; } };
+  }
+  if (id && typeof id.unref !== 'function') {
+    id.unref = () => id;
+    id.ref = () => id;
+  }
+  return id;
+};
+globalThis.setTimeout.call = _origSetTimeout.call?.bind(_origSetTimeout);
+globalThis.setTimeout.apply = _origSetTimeout.apply?.bind(_origSetTimeout);
+
+globalThis.setInterval = function(fn, ms, ...args) {
+  const id = _origSetInterval.call(this, fn, ms, ...args);
+  if (typeof id === 'number') {
+    return { _id: id, unref() { return this; }, ref() { return this; }, [Symbol.toPrimitive]() { return this._id; } };
+  }
+  if (id && typeof id.unref !== 'function') {
+    id.unref = () => id;
+    id.ref = () => id;
+  }
+  return id;
+};
+globalThis.setInterval.call = _origSetInterval.call?.bind(_origSetInterval);
+globalThis.setInterval.apply = _origSetInterval.apply?.bind(_origSetInterval);
